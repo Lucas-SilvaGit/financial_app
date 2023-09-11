33cb87015215980d863de86953d9d2112ae8d3eb
Jose Lucas <lucasnba2324@gmail.com>
2023-09-11 20:51:12 -0300

    fixing

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Accounts from './components/Accounts';
-import Categories from './components/Categories';
-import Entries from './components/Entries';
 
 const AppRouter = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/accounts" exact component={Accounts} />
-        <Route path="/categories" exact component={Categories} />
-        <Route path="/entries" exact component={Entries} />
       </Switch>
     </Router>
   );
