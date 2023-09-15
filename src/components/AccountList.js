32b81fa64133a7f6f82c6c508840f06dda08ce3a
Jose Lucas <lucasnba2324@gmail.com>
2023-09-15 06:57:20 -0300

    adding import for Api.js

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import api from '../Api'; 
 
 const AccountList = () => {
   const [accounts, setAccounts] = useState([]);
