b057203a65ca0fe3f993d86165c139d56413c364
Jose Lucas <lucasnba2324@gmail.com>
2023-09-15 06:57:07 -0300

    adding route for /accounts component and removing unnecesary div

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import AccountList from './components/AccountList';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <div>
-        <Route path="/" element={<Home />} />
-        <Route path="/" exact component={AccountList} />
-      </div>
+      <Route path="/" element={<Home />} />
+      <Route path="/accounts" element={<AccountList />} />
     </Routes>
   );
 };
