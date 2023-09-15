f289ac25d8b10c9a46a58aa79defa676ef6c3527
Jose Lucas <lucasnba2324@gmail.com>
2023-09-14 22:57:45 -0300

    creating account list component

---
+++ src/components/AccountList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const AccountList = () => {
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/v1/accounts')
+      .then(response => {
+        setAccounts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching accounts:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Lista de Contas</h2>
+      <ul>
+        {accounts.map(account => (
+          <li key={account.id}>{account.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AccountList;
