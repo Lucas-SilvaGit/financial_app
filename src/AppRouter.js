ef239984ffeb3dab0974bce81187d64b10b2da05
Jose Lucas <lucasnba2324@gmail.com>
2023-09-16 20:15:55 -0300

    Configurating route for create accounts

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AccountList from './components/AccountList';
+import AccountForm from './components/AccountForm';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/accounts" element={<AccountList />} />
+      <Route path="/accounts/create" element={<AccountForm />} />
     </Routes>
   );
 };
