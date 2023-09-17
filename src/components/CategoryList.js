d2d28d7ca41284722950b2142ce09c2ee7ceb970
Jose Lucas <lucasnba2324@gmail.com>
2023-09-17 08:01:19 -0300

    creating category list component

---
+++ src/components/CategoryList.js
@@ -0,0 +1,33 @@
+import React,  { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import axios from "axios";
+import api from "../Api";
+
+const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/v1/categories')
+      .then(response => {
+        setCategories(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching categories:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Lista de Categorias</h2>
+      <ul>
+        {categories.map(category => (
+          <li key={category.id}>
+            {category.description}{""}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryList;
