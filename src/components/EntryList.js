a98b01d2bcc95886e3bb0cdb2b4b1e65a8b45cc7
Jose Lucas <lucasnba2324@gmail.com>
2023-09-18 07:05:01 -0300

    creanting EntryList component

---
+++ src/components/EntryList.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const EntryList = () => {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/v1/entries`)
+      .then(response => {
+        setEntries(response.data);
+      })
+      .catch(error => {
+        console.error('Error getting entries:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Lista de Receitas e Despesas</h2>
+      <ul>
+        {entries.map(entry => (
+          <li key={entry.id}>
+            {entry.description}{" "}
+            {entry.value}{" "}
+            {entry.date}{" "}
+            {entry.billed}{" "}
+            {entry.entry_type}{" "}
+            {entry.category_id}{" "}
+
+            {console.log("entry.billed:", entry.billed)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default EntryList;
