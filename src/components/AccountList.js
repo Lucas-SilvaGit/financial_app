ff2eddf152fa433b4a904c8c1f097ec26fe88081
Jose Lucas <lucasnba2324@gmail.com>
2023-09-22 18:58:24 -0300

    removing unnecessary import

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import api from '../Api'; 
 
 const AccountList = () => {
   const [accounts, setAccounts] = useState([]);
