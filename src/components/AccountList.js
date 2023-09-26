e2ef608738e33665b6a2c928995a81c3ae027d3a
Jose Lucas <lucasnba2324@gmail.com>
2023-09-25 22:34:40 -0300

    adding paginate methods with react-paginate

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -2,19 +2,39 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import DataTable from './DataTable';
+import ReactPaginate from 'react-paginate';
 
 const AccountList = () => {
   const [accounts, setAccounts] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const perPage = 5;
 
   useEffect(() => {
-    axios.get('http://localhost:3001/v1/accounts')
-      .then(response => {
+    const loadAccounts = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:3001/v1/accounts',
+          {
+            params: {
+              'page[number]': currentPage,
+              'page[size]': perPage,
+            },
+          }
+        );
         setAccounts(response.data);
-      })
-      .catch(error => {
+
+        const totalPagesHeader = response.headers['x-total-pages'];
+        setTotalPages(parseInt(totalPagesHeader, 10));
+    
+      } catch (error) {
         console.error('Error fetching accounts:', error);
-      });
-  }, []);
+      }
+    };
+  
+    loadAccounts();
+  }, [currentPage]);
+  
 
   const handleDeleteClick = (accountId) => {
     if (window.confirm('Tem certeza de que deseja excluir esta conta?')) {
@@ -52,10 +72,27 @@ const AccountList = () => {
     },
   ];
 
+  const handlePageChange = ({ selected }) => {
+    const newPage = selected + 1;
+    setCurrentPage(newPage);
+  };
+
   return (
     <div className='container-lg'>
       <h2>Lista de Contas</h2>
       <DataTable data={accounts} columns={columns} />
+
+      <ReactPaginate
+        previousLabel={'Anterior'}
+        nextLabel={'Próximo'}
+        breakLabel={'...'}
+        pageCount={totalPages}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
 
       <Link to="/accounts/create" className="btn btn-success mt-3">
         Criar Nova Conta
