fb6bf89cc4e149a744a4f738aec99dbd0e62299e
Jose Lucas <lucasnba2324@gmail.com>
2023-09-30 23:14:02 -0300

    adding bootstrap to the create form

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -22,38 +22,64 @@ const AccountForm = () => {
       });
   };
 
-  return (
-    <div>
-      <h2>Criar Nova Conta</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nome da Conta:</label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Nome da Conta"
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="balance">Saldo:</label>
-          <input
-            type="number"
-            id="balance"
-            step="0.01"
-            placeholder="Saldo"
-            value={balance}
-            onChange={() => {}}
-            disabled
-          />
-        </div>
-        <button type="submit" className="btn btn-primary mt-3">Criar</button>
+  const containerStyle = {
+    maxWidth: '700px',
+    margin: '0 auto',
+  };
 
-        <Link to="/accounts" className="btn btn-warning mt-3">
-          Voltar
-        </Link>
-      </form>
+  const handleButtonClick = () => {
+    navigate('/accounts');
+  };
+
+  return (
+    <div className="container-lg mt-5" style={containerStyle}>
+      <div className="card shadow">
+        <div className="card-body">
+          <h2 className="card-title mt-3 mb-4">Criar Nova Conta</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="row mb-3 d-flex justify-content-center">
+              <label htmlFor="name" className="col-sm-3 col-form-label">
+                Nome da Conta:
+              </label>
+              <div className="col-sm-6">
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Nome da Conta"
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                  className="form-control"
+                />
+              </div>
+            </div>
+
+            <div className="row mb-3 d-flex justify-content-center">
+              <label htmlFor="balance" className="col-sm-3 col-form-label">
+                Saldo:
+              </label>
+              <div className="col-sm-6">
+                <input
+                  type="number"
+                  id="balance"
+                  step="0.01"
+                  placeholder="Saldo"
+                  value={balance}
+                  onChange={() => {}}
+                  disabled
+                  className="form-control"
+                />
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <div className="col-sm-10 offset-sm-4">
+                <button type="submit" className="btn btn-primary me-2">Criar</button>
+                <button className="btn btn-warning" onClick={handleButtonClick}>Voltar</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
