76030af80bcc7101e36d1569f31d9e86a320c75f
Jose Lucas <lucasnba2324@gmail.com>
2023-10-01 10:33:30 -0300

    adding bootstrap to entry edit form

--- src/components/EntryEdit.js
+++ src/components/EntryEdit.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const EntryEdit = () => {
   const { id } = useParams();
@@ -79,100 +79,137 @@ const EntryEdit = () => {
   };
 
   return (
-    <div>
-      <h2>Editar Conta</h2>
-      <form onSubmit={handleSubmit}>
-        {dataLoaded ? (
-          <div>
-            <div>
-              <label htmlFor="description">Descrição:</label>
-              <input
-                type="text"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </div>
-  
-            <div>
-              <label htmlFor="value">Valor:</label>
-              <input
-                type="number"
-                value={value}
-                onChange={(e) => setValue(parseFloat(e.target.value))}
-              />
-            </div>
-  
-            <div>
-              <label htmlFor="date">Data:</label>
-              <input
-                type="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-              />
-            </div>
-  
-            <div>
-              <label htmlFor="billed">Quitado:</label>
-              <input
-                type="checkbox"
-                checked={billed}
-                onChange={(e) => setBilled(e.target.checked)}
-              />
-            </div>
-  
-            <div>
-              <label htmlFor="entry_type">Tipo:</label>
-              <select
-                value={entry_type}
-                onChange={(e) => setEntryType(e.target.value)}
-              >
-                <option value="income">Receita</option>
-                <option value="expense">Despesa</option>
-              </select>
-            </div>
-  
-            <div>
-              <label htmlFor="category_id">Categoria:</label>
-              <select
-                value={category_id}
-                onChange={(e) => setCategoryId(e.target.value)}
-              >
-                <option value="">Selecione uma categoria</option>
-                {categories.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.description}
-                  </option>
-                ))}
-              </select>
-            </div>
-  
-            <div>
-              <label htmlFor="account_id">Conta:</label>
-              <select
-                value={account_id}
-                onChange={(e) => setAccountId(e.target.value)}
-              >
-                <option value="">Selecione uma conta</option>
-                {accounts.map((account) => (
-                  <option key={account.id} value={account.id}>
-                    {account.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-  
-            <button type="submit" className="btn btn-primary mt-3">
-              Atualizar
-            </button>
-  
-            <Link to="/entries" className="btn btn-warning mt-3">
-              Voltar
-            </Link>
-          </div>
-        ) : (
-          <p>Carregando dados...</p>
-        )}
-      </form>
+    <div className="container-lg mt-5" style={containerStyle}>
+      <div className="card shadow">
+        <div className="card-body">
+          <h2 className="card-title mt-3 mb-4">Editar Entrada</h2>
+          <form onSubmit={handleSubmit}>
+            {dataLoaded ? (
+              <div>
+                <div className="row mb-3 d-flex justify-content-center">
+                  <label htmlFor="description" className="col-sm-3 col-form-label">
+                    Descrição:
+                  </label>
+                  <div className="col-sm-6">
+                    <input
+                      type="text"
+                      placeholder="Descrição da entrada"
+                      value={description}
+                      onChange={e => setDescription(e.target.value)}
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div className="row mb-3 d-flex justify-content-center">
+                  <label htmlFor="value" className="col-sm-3 col-form-label">
+                    Valor:
+                  </label>
+                  <div className="col-sm-6">
+                    <input
+                      type="number"
+                      value={value}
+                      onChange={e => setValue(parseFloat(e.target.value))}
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div className="row mb-3 d-flex justify-content-center">
+                  <label htmlFor="date" className="col-sm-3 col-form-label">
+                    Data:
+                  </label>
+                  <div className="col-sm-6">
+                    <input
+                      type="date"
+                      value={date}
+                      onChange={e => setDate(e.target.value)}
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+      
+                <div className="row mb-3 d-flex justify-content-center">
+                  <label htmlFor="billed" className=" form-check-label col-sm-2">
+                    Efetuado:
+                  </label> 
+                  <div className="col-sm-1">
+                    <input
+                      type="checkbox"
+                      checked={billed}
+                      onChange={e => setBilled(e.target.checked)}
+                      className="form-check-input"
+                    />
+                  </div>
+                
+                  <label htmlFor="entry_type" className="col-sm-2 form-label">
+                    Tipo:
+                  </label>
+                  <div className="col-sm-3">
+                    <select
+                      value={entry_type}
+                      onChange={e => setEntryType(e.target.value)}
+                      className="form-select"
+                    >
+                      <option value="revenue">Receita</option>
+                      <option value="expense">Despesa</option>
+                    </select>
+                  </div>
+                </div>
+      
+                <div className="row mb-3 d-flex justify-content-center">
+                  <label htmlFor="category_id" className="col-sm-3 col-form-label">
+                    Categoria:
+                  </label>
+                  <div className="col-sm-6">
+                    <select
+                      value={category_id}
+                      onChange={e => setCategoryId(e.target.value)}
+                      className="form-select"
+                    >
+                      <option value="">Selecione uma Categoria</option>
+                        {categories.map(category => (
+                      <option key={category.id} value={category.id}>
+                        {category.description}
+                      </option>
+                        ))}
+                    </select>
+                  </div>
+                </div>
+      
+                <div className="row mb-3 d-flex justify-content-center">
+                  <label htmlFor="account_id" className="col-sm-3 col-form-label">
+                    Conta:
+                  </label>
+                  <div className="col-sm-6">
+                    <select
+                      value={account_id}
+                      onChange={e => setAccountId(e.target.value)}
+                      className="form-select"
+                    >
+                      <option value="">Selecione uma Conta</option>
+                      {accounts.map(account => (
+                        <option key={account.id} value={account.id}>
+                          {account.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                
+                <div className="row mb-3">
+                  <div className="col-sm-10 d-flex justify-content-end">
+                    <button type="submit" className="btn btn-primary me-2">Atualizar</button>
+                    <button className="btn btn-warning" onClick={handleButtonClick}>Voltar</button>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <p>Carregando dados...</p>
+            )}
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
