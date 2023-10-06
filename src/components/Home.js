e603874f017de3adc4ffc11956477a34bc496cef
Jose Lucas <lucasnba2324@gmail.com>
2023-10-05 22:12:51 -0300

    removing unecessary links

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,18 +5,6 @@ const Home = () => {
   return (
     <div>
       <h1> Bem-vindo a pagina inicial</h1>
-
-      <Link to="/accounts" className="btn btn-primary mt-3">
-        Acessar Contas
-      </Link>
-
-      <Link to="/categories" className="btn btn-primary mt-3">
-        Acessar Categorias
-      </Link>
-
-      <Link to="/entries" className="btn btn-primary mt-3">
-        Acessar Receitas e Despesas
-      </Link>
     </div>
   );
 };
