56d57d8235d7b809c3a29bace75629cfc92c413c
Jose Lucas <lucasnba2324@gmail.com>
2023-10-11 22:40:43 -0300

    importing tabler css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppRouter from './AppRouter';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/NavBar';
+import 'tabler-react/dist/Tabler.css';
 
 function App() {
   return (
